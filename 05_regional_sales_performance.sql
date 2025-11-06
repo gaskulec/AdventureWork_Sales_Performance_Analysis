84320dc98d31a6cbe26c08d6445e51516a381fe9
gaskulec <gabrielakozubal@gmail.com>
2025-11-06 19:46:05 +0100

    05_regional_sales_performance.sql

--- 05_regional_sales_performance.sql
+++ 05_regional_sales_performance.sql
@@ -1,10 +1,7 @@
 select st.[Group] as [Region], st.[CountryRegionCode], st.[Name] as [SalesTerritory],
 	round (sum(soh.TotalDue),2) as [TotalAmount],
-	count (soh.[SalesOrderID]) as [NumberOfOrders],
-	sum ([OrderQty]) as [NumberOfSoldProducts]
-from [Sales].[SalesOrderDetail] sod 
-join [Sales].[SalesOrderHeader] soh
-on sod.SalesOrderID = soh.SalesOrderID
+	count (soh.[SalesOrderID]) as [NumberOfOrders]
+from [Sales].[SalesOrderHeader] soh
 join [Sales].[SalesTerritory] st
 on soh.TerritoryID = st.TerritoryID
 group by st.[Group], st.[CountryRegionCode], st.[Name]
